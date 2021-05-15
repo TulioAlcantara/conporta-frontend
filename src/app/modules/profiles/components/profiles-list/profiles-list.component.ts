b7b2c569079b1ec4a31c029b502d0545dbd71081
tulio <tulio.alcantara@imaginie.com>
2021-05-15 16:46:54 -0300

    Criada de lista de profile;

--- src/app/modules/profiles/components/profiles-list/profiles-list.component.ts
+++ src/app/modules/profiles/components/profiles-list/profiles-list.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, tap } from 'rxjs/operators';
+import { Profile } from '../../../../shared/models/profile';
+import { ProfilesService } from '../../profiles.service';
 
 @Component({
   selector: 'app-profiles-list',
   templateUrl: './profiles-list.component.html',
-  styleUrls: ['./profiles-list.component.scss']
+  styleUrls: ['./profiles-list.component.scss'],
 })
 export class ProfilesListComponent implements OnInit {
+  searchFilterFormControl = new FormControl();
+  searchFilter: string = '';
+  isLoading: boolean = true;
+  ordinancesList: Profile[] = [];
+  displayedColumns: string[] = ['id', 'name', 'email', 'is_active'];
 
-  constructor() { }
+  constructor(public profileService: ProfilesService) {}
 
   ngOnInit(): void {
+    this.loadAllProfiles();
+    this.subscribeToSearchFilter();
   }
 
+  loadAllProfiles() {
+    this.profileService
+      .loadAllProfiles(this.searchFilter)
+      .subscribe((paginatedResponse) => {
+        this.ordinancesList = paginatedResponse.results;
+        this.isLoading = false;
+      });
+  }
+
+  subscribeToSearchFilter() {
+    this.isLoading = true;
+    this.searchFilterFormControl.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap((filterInput) => (this.searchFilter = filterInput))
+      )
+      .subscribe(() => this.loadAllProfiles());
+  }
 }
