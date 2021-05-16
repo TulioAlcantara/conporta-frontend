79414dc9c74c3172a3a2e7ca1a6667700a4d7019
tulio <tulio.alcantara@imaginie.com>
2021-05-16 05:30:21 -0300

    Corrigido bug de autocomplete do material;

--- src/app/modules/admin-units/components/admin-unit-info/admin-unit-info.component.ts
+++ src/app/modules/admin-units/components/admin-unit-info/admin-unit-info.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { ActivatedRoute } from "@angular/router";
-import { debounceTime } from "rxjs/operators";
-import Utils from "src/app/shared/utils";
-import { AuthService } from "../../../../auth/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+import Utils from 'src/app/shared/utils';
+import { AuthService } from '../../../../auth/auth.service';
 import {
   AdminUnit,
   AdminUnitMember,
@@ -12,14 +12,14 @@ import {
   AdminUnitTypeEnum,
   PartialAdminUnit,
   PartialAdminUnitMember,
-} from "../../../../shared/models/admin-unit";
-import { Profile } from "../../../../shared/models/profile";
-import { AdminUnitsService } from "../../admin-units.service";
+} from '../../../../shared/models/admin-unit';
+import { Profile } from '../../../../shared/models/profile';
+import { AdminUnitsService } from '../../admin-units.service';
 
 @Component({
-  selector: "app-admin-unit-info",
-  templateUrl: "./admin-unit-info.component.html",
-  styleUrls: ["./admin-unit-info.component.scss"],
+  selector: 'app-admin-unit-info',
+  templateUrl: './admin-unit-info.component.html',
+  styleUrls: ['./admin-unit-info.component.scss'],
 })
 export class AdminUnitInfoComponent implements OnInit {
   keys = Object.keys;
@@ -31,29 +31,30 @@ export class AdminUnitInfoComponent implements OnInit {
   adminUnitMembers: AdminUnitMember[] = [];
   nonMemberProfilesOptions: Profile[] = [];
   displayedColumns: string[] = [
-    "id",
-    "profile_name",
-    "type",
-    "is_boss",
-    "description",
-    "start_date",
-    "end_date",
+    'id',
+    'profile_name',
+    'type',
+    'is_boss',
+    'description',
+    'start_date',
+    'end_date',
   ];
-  adminUnitMemberType = AdminUnitMemberType;
+  adminUnitMemberTypeEnum = AdminUnitMemberType;
+  adminUnitMemberTypeSelect = Utils.enumEntriesToSelect(AdminUnitMemberType);
   adminUnitFormGroup = this.formBuilder.group({
-    id: [""],
-    name: ["", Validators.required],
-    initials: ["", Validators.required],
-    type: ["", Validators.required],
-    expedition_year: ["", Validators.required],
+    id: [''],
+    name: ['', Validators.required],
+    initials: ['', Validators.required],
+    type: ['', Validators.required],
+    expedition_year: ['', Validators.required],
   });
   newAdminUnitMemberFormGroup = this.formBuilder.group({
-    profile: ["", Validators.required],
-    is_boss: ["", Validators.required],
-    start_date: ["", Validators.required],
-    end_date: ["", Validators.required],
-    type: ["", Validators.required],
-    description: ["", Validators.required],
+    profile: ['', Validators.required],
+    is_boss: ['', Validators.required],
+    start_date: ['', Validators.required],
+    end_date: ['', Validators.required],
+    type: ['', Validators.required],
+    description: ['', Validators.required],
   });
 
   get isNewAdminUnit() {
@@ -72,7 +73,7 @@ export class AdminUnitInfoComponent implements OnInit {
     console.log(this.authService.userProfile);
     this.subscribeProfileAutoComplete();
     this.route.params.subscribe((params) => {
-      this.adminUnitId = +params["id"];
+      this.adminUnitId = +params['id'];
 
       if (this.adminUnitId != 0) {
         this.loadAdminUnit();
@@ -83,6 +84,7 @@ export class AdminUnitInfoComponent implements OnInit {
     });
   }
 
+  //TODO: FAZER ESSE MÉTODO JA TRAZER OS MEMBROS EM UMA UNICA CHAMADA
   loadAdminUnit(): void {
     this.adminUnitsService
       .loadAdminUnit(this.adminUnitId)
@@ -94,11 +96,13 @@ export class AdminUnitInfoComponent implements OnInit {
   }
 
   loadAdminUnitMembers(): void {
+    this.isLoading = true;
     this.adminUnitsService
       .loadMembers(this.adminUnitId)
       .subscribe((adminUnitMembers) => {
         this.adminUnitMembers = adminUnitMembers.results;
         console.log(this.adminUnitMembers);
+        this.isLoading = false;
       });
   }
 
@@ -109,8 +113,8 @@ export class AdminUnitInfoComponent implements OnInit {
         .updateAdminUnit(new PartialAdminUnit(this.adminUnitFormGroup.value))
         .subscribe(() => {
           this.snackBar.open(
-            "Unidade administrativa salva com sucesso!",
-            "FECHAR",
+            'Unidade administrativa salva com sucesso!',
+            'FECHAR',
             {
               duration: 5000,
             }
@@ -124,8 +128,8 @@ export class AdminUnitInfoComponent implements OnInit {
       .createAdminUnit(new PartialAdminUnit(this.adminUnitFormGroup.value))
       .subscribe((newAdminUnit) => {
         this.snackBar.open(
-          "Unidade administrativa criada com sucesso!",
-          "FECHAR",
+          'Unidade administrativa criada com sucesso!',
+          'FECHAR',
           {
             duration: 5000,
           }
@@ -143,7 +147,7 @@ export class AdminUnitInfoComponent implements OnInit {
       .loadProfilesThatArentMembers(this.adminUnitId)
       .subscribe((profiles) => {
         if (!profiles) {
-          this.snackBar.open("Nenhum perfil encontrado!", "FECHAR", {
+          this.snackBar.open('Nenhum perfil encontrado!', 'FECHAR', {
             duration: 5000,
           });
           this.isAddingMember = false;
@@ -158,10 +162,12 @@ export class AdminUnitInfoComponent implements OnInit {
       this.newAdminUnitMemberFormGroup.value
     );
     newMember.admin_unit = this.adminUnitId;
-    newMember.profile = 0;
-    newMember.is_boss = false;
+    newMember.profile = this.newAdminUnitMemberFormGroup.controls[
+      'profile'
+    ].value.id;
+    newMember.is_boss = newMember.is_boss || false;
     this.adminUnitsService.saveNewMember(newMember).subscribe(() => {
-      this.snackBar.open("Novo membro adicionado com sucesso!", "FECHAR", {
+      this.snackBar.open('Novo membro adicionado com sucesso!', 'FECHAR', {
         duration: 5000,
       });
       this.isAddingMember = false;
@@ -175,8 +181,7 @@ export class AdminUnitInfoComponent implements OnInit {
   }
 
   showProfileName(profile: Profile): string {
-    if (profile) return profile.name;
-    return "";
+    return profile ? profile.name : '';
   }
 
   subscribeProfileAutoComplete() {
